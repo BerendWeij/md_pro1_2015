b68e39a0569eb62d021bd99363a4bbb635e5e2b1
Berend Weij <info@berendweij.nl>
2015-10-05 11:55:40 +0200

    added our ImageGallery

--- les_5/js/main.js
+++ les_5/js/main.js
@@ -1,5 +1,19 @@
 window.addEventListener("load", function(){
 
-    
+    // we maken een xmlHTTPRequest aan
+    var req = new XMLHttpRequest();
+    // welke url moet er worden opgehaald?
+    req.open('GET', 'data/student.json', true);
 
+    // we voegen een 'listener' toe om te kijken of de pagina geladen is
+    // je kunt ook andere events gebruiken zoals readystatechange
+    req.addEventListener('load', function () {
+        var jsonString = req.responseText;
+        console.log(JSON.parse(jsonString));
+        console.log('bestand is binnen');
+    });
+
+    // we starten het laden van de pagina
+    req.send();
+    console.log('we halen het bestand op');
 });
